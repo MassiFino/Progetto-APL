b3734579323deb098f8960e883d6dedfd6cc11e9
Massi <massi.fino.001@gmail.com>
2024-12-02 16:51:44 +0100

    struttura_iniziale

---
+++ Interfaccia_C#/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Interfaccia_C_;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Interfaccia_C#/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Interfaccia_C_;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
