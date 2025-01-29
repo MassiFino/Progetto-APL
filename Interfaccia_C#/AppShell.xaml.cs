a728aae6a76b94eb665c1c194db1f8a737c35a71
DarioRovito <RVTDRA00B11C342W@studium.unict.com>
2025-01-29 03:04:23 +0100

    fixGeneraleMenu

--- Interfaccia_C#/AppShell.xaml.cs
+++ Interfaccia_C#/AppShell.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using Microsoft.Maui.Controls;
-using Interfaccia_C_.ViewModels;  // Assicurati di usare il namespace giusto
 
 namespace Interfaccia_C_
 {
     public partial class AppShell : Shell
     {
-        public AppShellViewModel ViewModel { get; set; }
 
         public AppShell()
         {
             InitializeComponent();
-            ViewModel = new AppShellViewModel(this); // Inizializza il ViewModel passando l'istanza di Shell
-            BindingContext = ViewModel;  // Imposta il BindingContext per l'AppShell
+           
         }
     }
 }
